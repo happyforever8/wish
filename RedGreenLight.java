1b98c9f62941cbad84185987e99b5ad80df598a1
happyforever8 <83077456+happyforever8@users.noreply.github.com>
2021-09-27 13:55:30 -0400

    Update RedGreenLight.java

--- RedGreenLight.java
+++ RedGreenLight.java
@@ -1,24 +1,38 @@
 给出一个array， 比如[红, 绿, 绿, 红, 红]. 翻转⼀个⼦区间(红变绿,绿变红),使得绿灯个数最⼤. 返回区间的起点和终点.
   
-  public int[] maxGreenLights(String s) {
-        int[] result = new int[2];
-        int count = 0, maxSum = 0, minSum = 0;
-        int left = -1;
-        for (int i = 0; i < s.length(); i++) {
-            if(s.charAt(i) == 'R') count++;
-            else count--;
-            if(count < minSum) {
-                left = i;
-                minSum = count;
-            }
-            if(count - minSum > maxSum) {
-                result[0] = left + 1;
-                result[1] = i ;
-                maxSum = count - minSum;
-            }
-        }
-        return result;
-    }
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// O means Green
+		// 1 means Red
+		String s = "011011";
+		String s1 = "101000110";
+		String s2 = "000";
+		for (int i : maxGreenLights(s2)){
+			System.out.println(i);
+		}
+
+	}
+	
+	 public static int[] maxGreenLights(String s) {
+	        int[] result = new int[2];
+	        int count = 0, maxSum = 0, minSum = 0;
+	        int left = -1;
+	        for (int i = 0; i < s.length(); i++) {
+	            if(s.charAt(i) == '1') count++;
+	            else count--;
+	            if(count < minSum) {
+	                left = i;
+	                minSum = count;
+	            }
+	            if(count - minSum > maxSum) {
+	                result[0] = left + 1;
+	                result[1] = i ;
+	                maxSum = count - minSum;
+	            }
+	        }
+	        return result;
+	    }
+
 
 双指针滑动窗口也行 说白了就是找一个区间 in which红的多余绿的差最大
 public static int[] solve(int[] input) {
