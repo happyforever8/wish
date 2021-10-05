057b8999878687b86488c86dfe62a53687aba463
happyforever8 <83077456+happyforever8@users.noreply.github.com>
2021-10-05 10:56:32 -0400

    Update Stock.java

--- Stock.java
+++ Stock.java
@@ -1,3 +1,44 @@
+implement a trading system which has the following components:
+1. buy(num_of_product, price)
+Called by buyer to buy certain number of products with a maximum price
+num_of_product (INTEGER): number of products to buy
+price (FLOAT): maximal price/unit the buyer is willing to pay
+Return (INTEGER): number of products can be bought
+
+2. sell(num_of_product, price)
+Called by seller to sell certain number of products with a minimum price
+num_of_product (INTEGER): number of products to sell
+price (FLOAT): minimal price/unit the seller is willing to sell
+Return (INTEGER): number of products can be sold
+
+Examples:
+system = TradingSystem()
+system.sell(50, 1.5)  
+return: 0
+system.sell(20, 1.4)  
+return: 0
+system.buy(60, 1.51) 
+return: 60
+system.buy(20, 1.5) 
+return: 10
+system.sell(20, 0.7)
+return: 10
+system.buy(100, 0.6)
+return: 0
+
+My Solution: Using Heaps
+
+Maintain min-heap for outstanding sellOrders
+Maintain max-heap for outstanding buyOrders
+When a buy order comes in match it with outstanding sell orders starting min selling price available, 
+i.e. from sellOrders until all requested no. of products can be fulfilled or min selling price available is >
+	buy price in the order. If can't fulfill the order add it in buyOrders heap with the remaining no. of items available.
+When a sell order comes in match it with outstanding buy order starting max buying price available, 
+i.e. from buyOrders until all requested no. of
+products can be fulfilled or max buying price available is < selling price in the order.
+	If can't fulfill the order add it in sellOrder heap with the remaining no. of items available.
+Wondering if there is a more optimized way to solve the problem using any other data structure.
+
 package wish;
 
 import java.util.Comparator;
